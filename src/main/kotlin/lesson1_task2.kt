b0acc8fa828442842da0c302021b2dbb66db24f1
checkbox2022 <ffsp500@gmail.com>
2024-02-22 00:12:10 +0300

    KS-1-2 init number of company employees and print it

---
+++ src/main/kotlin/lesson1_task2.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var numberOfOrders:Int = 75
+    val gratitude:String = "Thank you for visiting our store! Come to us again :)"
+    println(numberOfOrders)
+    println(gratitude)
+    //var numberOfCompanyEmployees:Int = 2000
+    //println(numberOfCompanyEmployees)
+    var numberOfCompanyEmployees:Int = 1999
+    println(numberOfCompanyEmployees)
+}
