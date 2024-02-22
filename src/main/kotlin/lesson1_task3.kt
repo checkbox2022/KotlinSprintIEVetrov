b02ba940b1ad8c82641fc253edebee60104fb794
checkbox2022 <ffsp500@gmail.com>
2024-02-22 21:10:51 +0300

    KS-1-3 Add functions "timeConvert" and "isTimeLessThenTen"

--- src/main/kotlin/lesson1_task3.kt
+++ src/main/kotlin/lesson1_task3.kt
@@ -3,10 +3,14 @@ fun main() {
     var hour = 9
     var minute = 7
     val name = "Yuri Gagarin"
+    var time = timeConvert(hour,minute)
     println("On April 12, $year, aboard the spacecraft Vostok 1, " +
             "27-year-old cosmonaut $name became the first man to travel into space." +
-            "\nTakeoff time: ${if (hour<10) "0" else ""}$hour:${if (hour<10) "0" else ""}$minute")
+            "\nTakeoff time: $time")
     hour = 10
     minute = 55
-    println("Landing time: ${if (hour<10) "0" else ""}$hour:${if (hour<10) "0" else ""}$minute")
+    time = timeConvert(hour,minute)
+    println("Landing time: $time")
 }
+fun isTimeLessThenTen (time:Int) = if (time<10) "0$time" else time.toString()
+fun timeConvert (hour:Int,minute:Int) = "time: ${isTimeLessThenTen(hour)}:${isTimeLessThenTen(minute)}"
