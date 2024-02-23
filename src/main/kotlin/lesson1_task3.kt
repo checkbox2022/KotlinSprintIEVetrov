4612ff0ade526a9975ef3509fe8780680828df7a
checkbox2022 <ffsp500@gmail.com>
2024-02-23 16:02:41 +0300

    KS-1-3 Refactor. Standart solve

--- src/main/kotlin/lesson1_task3.kt
+++ src/main/kotlin/lesson1_task3.kt
@@ -1,16 +1,11 @@
 fun main() {
-    val year = 1961
-    var hour = 9
-    var minute = 7
-    val name = "Yuri Gagarin"
-    var time = timeConvert(hour,minute)
-    println("On April 12, $year, aboard the spacecraft Vostok 1, " +
-            "27-year-old cosmonaut $name became the first man to travel into space." +
-            "\nTakeoff time: $time")
-    hour = 10
-    minute = 55
-    time = timeConvert(hour,minute)
-    println("Landing time: $time")
-    }
-fun isTimeLessThenTen (time:Int) = if (time<10) "0$time" else time.toString()
-fun timeConvert (hour:Int,minute:Int) = "time: ${isTimeLessThenTen(hour)}:${isTimeLessThenTen(minute)}"
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
+    println(year)
+    println(hour)
+    println(minute)
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
+}
