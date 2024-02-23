44374d07d894dbf772399f655d2bce5f1b67a34b
checkbox2022 <ffsp500@gmail.com>
2024-02-23 15:29:54 +0300

    KS-1-1 Task "variables". Refactor

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -1,6 +1,6 @@
 package lesson1
 
 fun main() {
-    var numberOfOrders:Int = 75
-    val gratitude:String = "Thank you for visiting our store! Come to us again :)"
+    val numberOfOrders: Int = 75
+    val gratitude: String = "Thank you for visiting our store! Come to us again :)"
 }
