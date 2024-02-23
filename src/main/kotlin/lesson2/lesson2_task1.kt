aad802e7ed359acdd4f02a152fc80041d61451c3
checkbox2022 <ffsp500@gmail.com>
2024-02-23 20:53:26 +0300

    KS-2-1 New conditions added to the function addStudentsGrade()

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -9,8 +9,7 @@ fun main() {
     println(average)
 }
 
-/*
-fun main() {
+/*fun main() {
     println(addStudentsGrade())
 
 }
@@ -19,9 +18,16 @@ fun addStudentsGrade(): Float {
     val studentsGrades = mutableListOf<Int>()
     println("Enter how many students are in the class:")
     val countOfStudents = readln().toInt()
+
     for (i in 1..countOfStudents) {
         println("Enter the grade of the student $i:")
-        studentsGrades.add(readln().toInt())
+        var grade = readln().toInt()
+        if (grade !in 1..5) {
+            println("the score can only contain values in the range from 1 to 5. Enter it again!")
+            grade = readln().toInt()
+        }
+
+        studentsGrades.add(grade)
     }
     return studentsGrades.sumOf { it } / countOfStudents.toFloat()
 }*/
