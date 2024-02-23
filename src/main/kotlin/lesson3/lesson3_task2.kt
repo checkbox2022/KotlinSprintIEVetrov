b47000f65167bf8b3fd3015823f421543eb2b494
checkbox2022 <ffsp500@gmail.com>
2024-02-23 23:00:26 +0300

    KS-3-2 App citizen data

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package lesson3
+
+fun main() {
+    var yearsOld = 20
+    val firstName = "Tatyana"
+    var lastName = "Andreeva"
+    val surname = "Sergeevna"
+    println("$lastName $firstName $surname, $yearsOld")
+    yearsOld = 22
+    lastName = "Sidorova"
+    println("$lastName $firstName $surname, $yearsOld")
+
+}
