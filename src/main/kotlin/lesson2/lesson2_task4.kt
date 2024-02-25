e48a733119f3ac00bcc003b3500d7ce1b0b84704
checkbox2022 <ffsp500@gmail.com>
2024-02-25 16:02:52 +0300

    Lesson1 task5 refactor-2

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,10 +1,15 @@
 package lesson2
 
+const val PERCENTAGE_BONUS = 20
+
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val crystalAbove = (crystalOre * 1.2 - crystalOre).toInt().toByte()
-    val ironAbove = (ironOre * 1.2 - ironOre).toInt().toByte()
+    val percentageBonusCoefficient = (PERCENTAGE_BONUS+100)/100.toFloat()
+
+    val crystalAbove = (crystalOre * percentageBonusCoefficient - crystalOre).toInt().toByte()
+    val ironAbove = (ironOre * percentageBonusCoefficient - ironOre).toInt().toByte()
+
     println("Extra crystal: $crystalAbove")
     println("Extra iron: $ironAbove")
 }
