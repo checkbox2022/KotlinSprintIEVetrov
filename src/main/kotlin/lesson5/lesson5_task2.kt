0738544148b4c06ac4d997389e5096bd3ea49e99
checkbox2022 <ffsp500@gmail.com>
2024-02-25 16:52:59 +0300

    KS-5-2 Majority

---
+++ src/main/kotlin/lesson5/lesson5_task2.kt
@@ -0,0 +1,16 @@
+package lesson5
+
+const val MAJORITY = 18
+const val YEAR = 2024
+
+fun main() {
+    val userYearOfBirth = readln().toInt()
+
+    val userAge = YEAR - userYearOfBirth
+    val isUserAgeMajority = userAge >= MAJORITY
+    val adultContent =
+        if (isUserAgeMajority) "Access to hidden content is opened!"
+        else "Access to hidden content is closed!"
+
+    println(adultContent)
+}
