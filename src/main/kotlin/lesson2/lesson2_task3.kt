d720d4de23d5e97c88ed93c0ca8183b36b56befe
checkbox2022 <ffsp500@gmail.com>
2024-02-26 13:02:47 +0300

    KS-2-3-Trains refactor

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,14 +1,16 @@
 package lesson2
+const val TO_MIN_OR_SEC = 60
+const val MINUTE_TO_HOUR = 60
 
 fun main() {
     val departureHours = 9
     val departureMinutes = 39
     val travelTimeMinutes = 457
-    var arrivalHours = departureHours + (travelTimeMinutes / 60)
+    var arrivalHours = departureHours + (travelTimeMinutes / TO_MIN_OR_SEC)
     //println(arrivalHours)
-    val arrivalMinutes = if ((departureMinutes + travelTimeMinutes % 60) >= 60) {
+    val arrivalMinutes = if ((departureMinutes + travelTimeMinutes % TO_MIN_OR_SEC) >= MINUTE_TO_HOUR) {
         arrivalHours++
-        (departureMinutes + travelTimeMinutes % 60) % 60
-    } else departureMinutes + travelTimeMinutes % 60
+        (departureMinutes + travelTimeMinutes % TO_MIN_OR_SEC) % TO_MIN_OR_SEC
+    } else departureMinutes + travelTimeMinutes % TO_MIN_OR_SEC
     println("Train arrival time: $arrivalHours:$arrivalMinutes")
 }
