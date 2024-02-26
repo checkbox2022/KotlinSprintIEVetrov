0e4d525db15689450b6a3b05615532debdef73e5
checkbox2022 <ffsp500@gmail.com>
2024-02-26 18:27:32 +0300

    KS-4-3 Plants (updated)

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,12 +1,18 @@
 package lesson4
+const val FAVORABLE_PERCENTAGE_FOR_GROWTH = 20
 
 fun main() {
     val isWeatherSunny = true
     val isAwningOpen = true
     val percentageAirHumidity = 20
     val season = "Winter"
+
     val isFriendlyConditionForGrowth =
-        if (isWeatherSunny == true && isAwningOpen == true && percentageAirHumidity == 20 && season != "Winter"
-        ) true else false
+        if (isWeatherSunny == true && isAwningOpen == true
+            && percentageAirHumidity == FAVORABLE_PERCENTAGE_FOR_GROWTH
+            && season != "Winter"
+        ) true
+        else false
+
     println("Are the conditions now favorable for the growth of legumes: $isFriendlyConditionForGrowth")
 }
