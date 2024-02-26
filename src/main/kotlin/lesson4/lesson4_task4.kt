ca3456b6df33a617f926627595c2f3d2ca9b3d09
checkbox2022 <ffsp500@gmail.com>
2024-02-26 18:29:57 +0300

    KS-4-4 Training (updated)

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -6,10 +6,12 @@ fun main() {
     var isLegs = false
     var isBack = false
     var isPress = false
-    val group = if (trainingDay % 2 != 0) {
+
+    if (trainingDay % 2 != 0) {
         isHands = true;isPress = true
     } else {
         isLegs = true;isBack = true
     }
+
     println("Hands: $isHands\nLegs:  $isLegs\nBack:  $isBack\nPress: $isPress")
 }
