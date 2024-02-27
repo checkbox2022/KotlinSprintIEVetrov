1dc9054b3563701c6279485c88dc19a587136319
checkbox2022 <ffsp500@gmail.com>
2024-02-27 12:31:52 +0300

    KS-4-1 Restaurant refactor

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -1,10 +1,10 @@
 package lesson4
+const val TOTAL_NUMBER_TABLES = 13
 
 fun main() {
-    val totalNumberOfTables = 13
     val todayBookedTables = 13
     val tomorrowBookedTables = 9
 
-    println("available tables for today: ${if (totalNumberOfTables - todayBookedTables > 0) true else false}")
-    println("available tables for tomorrow: ${if (totalNumberOfTables - tomorrowBookedTables > 0) true else false}")
+    println("available tables for today: ${TOTAL_NUMBER_TABLES > todayBookedTables}")
+    println("available tables for tomorrow: ${TOTAL_NUMBER_TABLES > tomorrowBookedTables}")
 }
