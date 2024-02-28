50a69d74fa3f8042345b99a55c1957fc3a78323e
checkbox2022 <ffsp500@gmail.com>
2024-02-28 11:30:15 +0300

    KS-5-1 Authorization (updated)

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,11 +1,13 @@
 package lesson5
 const val ACCESS_IS_OPEN = "You are WELCOME! ^^"
 const val ACCESS_IS_CLOSED = "Access is CLOSED!"
+const val ACCESS_RES = 15
 
 fun main() {
     println("Solve the equation and write the answer in the input field:\n7 + 11 - 3 = ?")
     val res = readln().toInt()
 
-    val accessStatus = if (res == 15) ACCESS_IS_OPEN else ACCESS_IS_CLOSED
+    val accessStatus = if (res == ACCESS_RES) ACCESS_IS_OPEN else ACCESS_IS_CLOSED
+
     println(accessStatus)
 }
