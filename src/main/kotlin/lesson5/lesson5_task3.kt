2d3e0a9eb7e6230a66857403139bd89ce833603e
checkbox2022 <ffsp500@gmail.com>
2024-02-28 16:37:06 +0300

    KS-5-3 Lottery (updated)

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -1,19 +1,18 @@
 package lesson5
+const val FIRST_NUMBER = 15
+const val SECOND_NUMBER = 33
 
 fun main() {
-    val firstNumber = 15
-    val secondNumber = 33
-
     println("Enter two random numbers from 0 to 42.\nEnter first number:")
     val firstEnter = readln().toInt()
     println("Enter second number:")
     val secondEnter = readln().toInt()
 
-    if ((firstEnter == firstNumber || firstEnter == secondNumber) && (secondEnter == firstNumber || secondEnter == secondNumber))
+    if ((firstEnter == FIRST_NUMBER && secondEnter == SECOND_NUMBER) || (firstEnter == SECOND_NUMBER && secondEnter == FIRST_NUMBER))
         println("You won main prize! Gratz!")
-    else if ((firstEnter == firstNumber || firstEnter == secondNumber) || (secondEnter == firstNumber || secondEnter == secondNumber))
+    else if ((firstEnter == FIRST_NUMBER || firstEnter == SECOND_NUMBER) || (secondEnter == FIRST_NUMBER || secondEnter == SECOND_NUMBER))
         println("You won a consolation prize")
     else println("Failure")
 
-    println("\nThe random numbers were: $firstNumber and $secondNumber")
+    println("\nThe numbers conceived by the program were: $FIRST_NUMBER and $SECOND_NUMBER")
 }
