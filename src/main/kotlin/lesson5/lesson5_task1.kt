6ae790032c4b6155c492852a21b2033323f460a0
checkbox2022 <ffsp500@gmail.com>
2024-02-29 21:33:05 +0300

    KS-5-1 Authorization (new pull request)

---
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -0,0 +1,16 @@
+package lesson5
+
+const val ACCESS_IS_OPEN = "You are WELCOME! ^^"
+const val ACCESS_IS_CLOSED = "Access is CLOSED!"
+
+const val FIRST_NUM_CHECK = 4
+const val SECOND_NUM_CHECK = 11
+
+fun main() {
+    println("Solve the equation and write the answer in the input field:\n$FIRST_NUM_CHECK + $SECOND_NUM_CHECK = ?")
+    val res = readln().toInt()
+
+    val accessStatus = if (res == (FIRST_NUM_CHECK + SECOND_NUM_CHECK)) ACCESS_IS_OPEN else ACCESS_IS_CLOSED
+
+    println(accessStatus)
+}
