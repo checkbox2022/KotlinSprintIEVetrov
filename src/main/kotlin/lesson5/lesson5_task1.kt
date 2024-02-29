f3c9d61eac7645eaad8beae1a2729c0c78b8c4f8
checkbox2022 <ffsp500@gmail.com>
2024-02-29 18:20:24 +0300

    KS-5-1 Authorization refactor

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,10 +1,15 @@
 package lesson5
 const val ACCESS_IS_OPEN = "You are WELCOME! ^^"
 const val ACCESS_IS_CLOSED = "Access is CLOSED!"
+
+const val FIRST_NUM_CHECK = 7
+const val SECOND_NUM_CHECK = 11
+const val THIRD_NUM_CHECK = 3
 const val ACCESS_RES = 15
 
+
 fun main() {
-    println("Solve the equation and write the answer in the input field:\n7 + 11 - 3 = ?")
+    println("Solve the equation and write the answer in the input field:\n$FIRST_NUM_CHECK + $SECOND_NUM_CHECK - $THIRD_NUM_CHECK = ?")
     val res = readln().toInt()
 
     val accessStatus = if (res == ACCESS_RES) ACCESS_IS_OPEN else ACCESS_IS_CLOSED
