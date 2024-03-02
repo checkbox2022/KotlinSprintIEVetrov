305e31bdb1c2ad30f4ac6f4929aedd5271cd81a1
checkbox2022 <ffsp500@gmail.com>
2024-03-02 21:25:16 +0300

    KS-6-5 Registration and authorization + AntiBot (updated)

--- src/main/kotlin/lesson6/lesson6_task5.kt
+++ src/main/kotlin/lesson6/lesson6_task5.kt
@@ -8,14 +8,17 @@ const val LAST_ATTEMPT = 1
 fun main() {
     print("Create a login: ")
     val userLogin = readln()
+
     print("Create a password: ")
     val userPassword = readln()
 
     do {
         println("\nEnter your login:")
         val login = readln()
+
         println("Enter your password:")
         val password = readln()
+
     } while (login != userLogin || password != userPassword)
 
     var countAttempts = ATTEMPTS
