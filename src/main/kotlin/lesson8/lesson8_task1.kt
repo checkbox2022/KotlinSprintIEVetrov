ba2d8c4d51b7f1a807471f8803d469c6a9cbcaca
checkbox2022 <ffsp500@gmail.com>
2024-03-04 18:57:04 +0300

    KS-8-1 Sum Array Week refactor

--- src/main/kotlin/lesson8/lesson8_task1.kt
+++ src/main/kotlin/lesson8/lesson8_task1.kt
@@ -2,11 +2,6 @@ package lesson8
 
 fun main() {
     val arrViewWeek = intArrayOf(17, 22, 42, 11, 6, 37, 15)
-    var sumViews = 0
 
-    for (i in arrViewWeek) {
-        sumViews += i
-    }
-
-    println(sumViews)
+    println(arrViewWeek.sum())
 }
