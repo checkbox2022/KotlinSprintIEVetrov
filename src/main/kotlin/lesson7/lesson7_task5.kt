10ef7faabdafb47fd92452cddb939d681dcff746
checkbox2022 <ffsp500@gmail.com>
2024-03-04 19:59:07 +0300

    KS-7-5 Password AZaz09

---
+++ src/main/kotlin/lesson7/lesson7_task5.kt
@@ -0,0 +1,23 @@
+package lesson7
+
+const val UNIQUE_THREE_FIRST_SYMBOLS = 3
+const val MIN_PASSWORD_LENGTH = 6
+
+fun main() {
+    var password = ""
+    val passwordContent = listOf(('A'..'Z'), ('a'..'z'), ('0'..'9'))
+    var passwordLength = MIN_PASSWORD_LENGTH
+    println("Minimum password length is 6")
+
+    do {
+        print("Enter password length: ")
+        passwordLength = readln().toInt()
+    } while (passwordLength < MIN_PASSWORD_LENGTH)
+
+    password += passwordContent.map { it.random() }.joinToString("")
+
+    for (i in UNIQUE_THREE_FIRST_SYMBOLS until passwordLength)
+        password += (passwordContent[(0..2).random()].random())
+
+    println(password)
+}
