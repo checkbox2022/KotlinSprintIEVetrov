141511dfd3e70217b6682de9e93e21002ef5eec5
checkbox2022 <ffsp500@gmail.com>
2024-03-20 15:42:27 +0300

    KS-19-3 TODO Spaceship

---
+++ src/main/kotlin/lesson19/lesson19_task3.kt
@@ -0,0 +1,25 @@
+package lesson19
+
+fun main() {
+    val spaceship = Spaceship()
+
+    spaceship.shoot()
+    spaceship.land()
+    spaceship.takeOff()
+
+}
+
+class Spaceship {
+
+    fun takeOff() {
+        TODO()
+    }
+
+    fun land() { // TODO need information about additional logic
+    }
+
+    fun shoot() {
+        TODO("NotImplementedError")
+    }
+
+}
