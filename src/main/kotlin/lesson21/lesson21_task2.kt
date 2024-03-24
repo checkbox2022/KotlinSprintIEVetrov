8e7243e75b72ad1f8dc6f3e93fa9501b8ab11541
checkbox2022 <ffsp500@gmail.com>
2024-03-24 14:47:49 +0300

    KS-21-2 evenNumbersSum refactor

--- src/main/kotlin/lesson21/lesson21_task2.kt
+++ src/main/kotlin/lesson21/lesson21_task2.kt
@@ -1,19 +1,11 @@
 package lesson21
 
 fun main() {
+
     val listInt = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
     println(listInt.evenNumbersSum())
 
 }
 
-fun List<Int>.evenNumbersSum(): Int {
-    var sum = 0
-
-    subList(0, size).forEach {
-        if (it % 2 == 0) sum += it
-    }
-
-    return sum
-
-}
+fun List<Int>.evenNumbersSum() = filter { it % 2 == 0 }.sum()
