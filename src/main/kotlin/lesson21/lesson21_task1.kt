c7f25c5213a4b024c6d0495d76fe22e394d80952
checkbox2022 <ffsp500@gmail.com>
2024-03-24 14:42:12 +0300

    KS-21-1 vowelCount refactor

--- src/main/kotlin/lesson21/lesson21_task1.kt
+++ src/main/kotlin/lesson21/lesson21_task1.kt
@@ -3,18 +3,9 @@ package lesson21
 fun main() {
 
     val str = "Hello"
+
     println(str.vowelCount())
 
 }
 
-fun String.vowelCount(): Int {
-
-    var count = 0
-
-    subSequence(0, length).forEach {
-        if (it in "AaEeIiOoUu") count++
-    }
-
-    return count
-
-}
+fun String.vowelCount() = count { it in "AaEeIiOoUu" }
